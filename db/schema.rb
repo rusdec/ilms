3d517f356b9847ee216e2ea01c4cf9e18b6335dd
rusdec <ruskidecko@gmail.com>
2018-07-02 17:33:49 +0300

    model: add Role model +testing

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_02_125916) do
+ActiveRecord::Schema.define(version: 2018_07_02_135739) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "roles", force: :cascade do |t|
+    t.string "name", default: "user"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
