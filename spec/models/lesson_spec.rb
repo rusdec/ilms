0c50b4816e3a6258274b933225ae955bce6b32c5
rusdec <ruskidecko@gmail.com>
2018-07-08 17:47:13 +0300

    fix: add dependent destroy to lesson

--- spec/models/lesson_spec.rb
+++ spec/models/lesson_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe Lesson, type: :model do
   it { should validate_presence_of(:title) }
   it { should validate_length_of(:title).is_at_least(5).is_at_most(50) }
-  it { should belong_to(:course) }
+  it { should belong_to(:course).dependent(:destroy) }
   it { should belong_to(:author).with_foreign_key('user_id').class_name('User') }
   it do
     should validate_numericality_of(:order)
