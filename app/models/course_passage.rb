ae9c6bebb03a471523a31337e1156e0b77b6b91a
rusdec <ruskidecko@gmail.com>
2018-07-18 23:32:53 +0300

    159129824_user_begin_course_passage - add validates on already studing course - testing

--- app/models/course_passage.rb
+++ app/models/course_passage.rb
@@ -3,9 +3,17 @@ class CoursePassage < ApplicationRecord
   belongs_to :course
   has_many :lesson_passages
 
+  validate :validate_already_course_passage
+
   after_create :after_create_create_lesson_passage
 
   private
+
+  def validate_already_course_passage
+    if CoursePassage.find_by(educable: educable, course: course, passed: false)
+      errors.add(:course, 'is already on your study')
+    end
+  end
 
   def after_create_create_lesson_passage
     course.lessons.map do |lesson|
--- spec/models/course_passage_spec.rb
+++ spec/models/course_passage_spec.rb
@@ -26,4 +26,36 @@ RSpec.describe CoursePassage, type: :model do
       end
     end
   end
+
+  context '.validate_already_course_passage' do
+    let!(:course) { create(:course_master, :with_course_and_lessons).courses.last }
+    let(:user) { course.author }
+    let(:new_course_passage) do
+      build(:course_passage, course: course, educable: user)
+    end
+
+    context 'when user studing course' do
+      before { create(:course_passage, course: course, educable: user) }
+
+      it 'can\'t start studing it again' do
+        expect(new_course_passage).to_not be_valid
+      end
+    end
+
+    context 'when user passed course' do
+      before do
+        create(:course_passage, course: course, educable: user, passed: true)
+      end
+
+      it 'can start studing it again' do
+        expect(new_course_passage).to be_valid
+      end
+    end
+
+    context 'when user never took a course' do
+      it 'can start studing it again' do
+        expect(new_course_passage).to be_valid
+      end
+    end
+  end
 end
